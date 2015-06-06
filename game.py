eeacdecd41c6aad4e9ff959a482928619866ea95
Adam Jacobson <aj499@georgetown.edu>
2015-06-06 17:36:10 -0400

    Create game class

---
+++ game.py
@@ -0,0 +1,12 @@
+from constants import *
+
+STARTING_PLAYER = WHITE
+
+"""
+Represents a game
+Contains the canonical copy of the game's state
+"""
+class Game:
+    def __init__(self):
+        self.units = {}
+        pass
