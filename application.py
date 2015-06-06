87e84e9d5c7589e6464a6ed2040fd65750154838
Adam Jacobson <aj499@georgetown.edu>
2015-06-06 17:52:39 -0400

    Add route and template for website root

--- application.py
+++ application.py
@@ -2,4 +2,17 @@
 YOLO SWAG 420
 """
 
-pass
+from flask import Flask, abort, redirect, render_template, url_for
+
+ROOT_TEMPLATE = 'root.html'
+
+app = Flask(__name__)
+
+"""
+Render the root page
+Stub for now
+TODO: allow users to create/join games from here
+"""
+@app.route('/')
+def default_root():
+    return render_template(ROOT_TEMPLATE)
