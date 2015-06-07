4f65d117b228576b7be818a24d4240c61ab7afe5
Adam Jacobson <aj499@georgetown.edu>
2015-06-06 20:14:47 -0400

    Minor formatting change

--- game.py
+++ game.py
@@ -7,9 +7,11 @@ class Game:
     Represents a game
     Contains the canonical copy of the game's state
     """
+
     def __init__(self):
         """
         Set up a game with default conditions
+
         :return: a initialized Game object
         """
         self.units = {}
