759c8ab187a3d6a606e6602251af35246b05a465
Adam Jacobson <aj499@georgetown.edu>
2015-06-06 22:23:18 -0400

    Base work for passing moves back to the server
    Stub implementation of game_update_move()

--- application.py
+++ application.py
@@ -2,7 +2,9 @@
 YOLO SWAG 420
 """
 
-from flask import Flask, redirect, render_template, request
+from json import dumps
+
+from flask import Flask, redirect, render_template
 
 from game import Game
 
@@ -15,9 +17,9 @@ app = Flask(__name__)
 games = {}
 
 @app.route('/')
-def default_root():
+def render_root():
     """
-    Renders the root page.
+    Render the root page.
 
     TODO: allow users to create/join games from here
 
@@ -25,7 +27,7 @@ def default_root():
     """
     return render_template(ROOT_TEMPLATE)
 
-@app.route('/create_game/<int:game_id>')
+@app.route('/create/game/<int:game_id>')
 def create_game(game_id):
     """
     Create a new game with the given game_id.
@@ -46,7 +48,7 @@ def create_game(game_id):
 @app.route('/game/<int:game_id>')
 def game(game_id):
     """
-    Renders the page for a given game.
+    Render the page for a given game.
 
     If the indicated game does not exist a redirect to '/' will be returned.
 
@@ -57,6 +59,30 @@ def game(game_id):
     if game_id == None: return redirect('/')
 
     return render_template(GAME_TEMPLATE, game_id = game_id)
+
+@app.route('/update/game/<int:game_id>/move/<int:unit_id>/to/<int:x>/<int:y>')
+def game_update_move(game_id, unit_id, x, y):
+    """
+    Move a given unit in the given game to the given position.
+
+    The backend checks whether the move is valid and then returns the new game state. This state, or an error message if the move was illegal, is then returned.
+
+    :param game_id: the game containing the unit that is moving
+    :param unit_id: the id of the unit that is moving
+    :param x: the x-coordinate the piece is moving to
+    :param y: the y-coordinate the piece is moving to
+    :return: the game state after the move, as JSON
+    """
+    return format_error('default success!')
+
+def format_error(error_message):
+    """
+    Format an error message as a JSON object suitable for returning to the browser.
+
+    :param error_message: the error message to format
+    :return: a JSON object containing the error message
+    """
+    return dumps({'status': 'error', 'message': error_message})
 
 if __name__ == '__main__':
     app.run()
